5189c336a3a51d28fe46b917d4b3f4f35d59ccd4
TonyDorek <103887735+TonyDorek@users.noreply.github.com>
2025-10-04 13:49:01 +0200

    Add files via upload

---
+++ test/three_rob_two_obs/init_params3.m
@@ -0,0 +1,55 @@
+clear;
+clc;
+
+kx1 = 0.6;
+ky1 = 0.6;
+K1 = [kx1,0;
+    0,ky1];
+
+kx2 = 0.6;
+ky2 = 0.6;
+K2 = [kx2,0;
+    0,ky2];
+
+kx3 = 0.6;
+ky3 = 0.6;
+K3 = [kx3,0;
+    0,ky3];
+
+
+x01 = 0;
+y01 = 0;
+X01 = [x01;y01];
+x02 = 102;
+y02 = 56;
+X02 = [x02;y02];
+x03 = 50;
+y03 = -20;
+X03 = [x03; y03];    
+
+x1d = 102;
+y1d = 56;
+X1d = [x1d;y1d];
+x2d = 2;
+y2d = 2;
+X2d = [x2d;y2d];
+x3d = 40;
+y3d = 60;
+X3d = [x3d; y3d];      
+
+x1d_dot = 0;
+y1d_dot = 0;
+X1d_dot = [x1d_dot; y1d_dot];
+x2d_dot = 0;
+y2d_dot = 0;
+X2d_dot = [x2d_dot; y2d_dot];
+x3d_dot = 0;
+y3d_dot = 0;
+X3d_dot = [x3d_dot; y3d_dot];
+
+
+obj1 = [60, 30];    
+obj2 = [45, 25];    
+thr = 2;            
+
+
---
+++ test/three_rob_two_obs/visual3.m
@@ -0,0 +1,34 @@
+figure; hold on; grid on; axis equal;
+title('Animazione traiettorie');
+xlabel('X'); ylabel('Y');
+
+plot(obj1(1), obj1(2), '-s', 'MarkerSize', 10, 'MarkerFaceColor', 'red', 'MarkerEdgeColor', 'black');
+plot(obj2(1), obj2(2), '-s', 'MarkerSize', 10, 'MarkerFaceColor', 'blue', 'MarkerEdgeColor', 'black');
+
+
+plot(x01, y01, '-o', 'MarkerFaceColor', 'k');
+plot(x02, y02, '-o', 'MarkerFaceColor', 'k');
+plot(x03, y03, '-o', 'MarkerFaceColor', 'k');
+
+plot(x1d, y1d, '-x', 'MarkerSize', 8, 'LineWidth', 1.5);
+plot(x2d, y2d, '-x', 'MarkerSize', 8, 'LineWidth', 1.5);
+plot(x3d, y3d, '-x', 'MarkerSize', 8, 'LineWidth', 1.5);
+
+X1 = squeeze(out.X1.Data);
+X2 = squeeze(out.X2.Data);
+X3 = squeeze(out.X3.Data);
+
+h1 = plot(NaN, NaN, 'r', 'LineWidth', 2);
+h2 = plot(NaN, NaN, 'b', 'LineWidth', 2);
+h3 = plot(NaN, NaN, 'g', 'LineWidth', 2);
+
+legend('Obj1','Obj2','Start1','Start2','Start3','Goal1','Goal2','Goal3','Traj1','Traj2','Traj3');
+
+
+for i = 1:length(X1(1,:))
+    set(h1, 'XData', X1(1,1:i), 'YData', X1(2,1:i));
+    set(h2, 'XData', X2(1,1:i), 'YData', X2(2,1:i));
+    set(h3, 'XData', X3(1,1:i), 'YData', X3(2,1:i));
+    drawnow;
+    pause(0.2);
+end
