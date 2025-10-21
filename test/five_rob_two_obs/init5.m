0f2341b35e6eac317820e7e0e674012531b3a285
TonyDorek <dorecchiantonio91@gmail.com>
2025-10-21 12:00:10 +0200

    adding connectivity section

--- test/five_rob_two_obs/init5.m
+++ test/five_rob_two_obs/init5.m
@@ -1,8 +1,10 @@
 clear;
 clc;
 
-n_robots = 5;           
-thr = 2;               
+n_robots = 5;
+thr = 5;
+range = 100;
+eps = 0.01; % Disconnection: eps = 0;
 
 kx = 0.6 * ones(1, n_robots);
 ky = 0.6 * ones(1, n_robots);
@@ -16,13 +18,13 @@ for i = 1:n_robots
     eval(sprintf('K%d = K{%d};', i, i));
 end
 
-X0 = [  0, 102,  50, 160, 200;   
-        0,  56, -20,  40, 100];  
+X0 = [0, 102, 50, 100, 0;
+      0, 56, -20, 40, 20];
 
-Xd = [100,   2,  40, 40, 0;    
-       60,   4,  60,  40, 20];   
+Xd = [100, 2, 20, 40, 200;
+       60, 4, 40, 40, 100];
 
-Xd_dot = zeros(2, n_robots);  
+Xd_dot = zeros(2, n_robots);
 
 for i = 1:n_robots
     eval(sprintf('X%dd = Xd(:,%d);', i, i));       % crea X1d, X2d, X3d, X4d, X5d
@@ -30,5 +32,14 @@ for i = 1:n_robots
     eval(sprintf('X%02d = X0(:,%d);', i, i));       % opzionale: anche X01, X02, ecc.
 end
 
-obj1 = [60, 45];  
+obj1 = [60, 45];
 obj2 = [30, 25];
+
+figure;
+set(gcf, 'Position', [250, 250, 1200, 500]); % [x, y, width, height]
+
+subplot(1, 2, 1); % Create a 1x2 grid, access the 1st plot
+create_graph(X0,range,'Initial robot positions (nodes) and connections (edges)');
+
+subplot(1, 2, 2); % Access the 2nd plot
+create_graph(Xd,range,'Desired robot positions (nodes) and connections (edges)');
--- test/five_rob_two_obs/visualize5.m
+++ test/five_rob_two_obs/visualize5.m
@@ -1,62 +1,44 @@
-% clear;
-% clc;
-% 
-% %% === PARAMETRI INIZIALI ===
-% n_robots = 5;           
-% thr = 2;               
-% 
-% kx = 0.6 * ones(1, n_robots);
-% ky = 0.6 * ones(1, n_robots);
-% 
-% for i = 1:n_robots
-%     K{i} = [kx(i), 0;
-%              0, ky(i)];
-% end
-% 
-% %% === POSIZIONI INIZIALI ===
-% X0 = [  0, 102,  50, 160, 200;   
-%         0,  56, -20,  40, 100];  
-% 
-% %% === POSIZIONI DESIDERATE ===
-% Xd = [102,   2,  40, 180, 220;    
-%        56,   2,  60,  80, 120];   
-% 
-% %% === DERIVATE DESIDERATE ===
-% Xd_dot = zeros(2, n_robots);  
-% 
-% %% === OSTACOLI ===
-% obj1 = [60, 45];  
-% obj2 = [30, 25];  
+%% === ANIMATIONS ===
+figure; 
 
-%% === ANIMAZIONE ===
-figure; hold on; grid on; axis equal;
-title('Animazione traiettorie (5 Robot, 2 Ostacoli)');
-xlabel('X'); ylabel('Y');
+subplot(1, 2, 1);
 
-% --- Ostacoli ---
+hold on; grid on;% axis equal;
+ 
+set(gcf, 'Position', [250, 250, 1200, 500]); % [x, y, width, height]
+
+title('Safe multi-agent system: collision avoidance + connectivity maintenance');
+xlabel('x'); ylabel('y');
+
+% --- Obstacles ---
 plot(obj1(1), obj1(2), '-s', 'MarkerSize', 10, 'MarkerFaceColor', 'red', 'MarkerEdgeColor', 'black');
 plot(obj2(1), obj2(2), '-s', 'MarkerSize', 10, 'MarkerFaceColor', 'blue', 'MarkerEdgeColor', 'black');
 
+text(obj1(1), obj1(2), 'obj1', 'HorizontalAlignment', 'left', 'VerticalAlignment', 'top');
+text(obj2(1), obj2(2), 'obj2', 'HorizontalAlignment', 'left', 'VerticalAlignment', 'top');
+
 colors = ['r','b','g','m','c'];
 
-% --- Posizioni iniziali ---
+% --- Initial positions ---
 for i = 1:n_robots
     plot(X0(1,i), X0(2,i), '-o', 'MarkerFaceColor', colors(i));
+    text(X0(1,i), X0(2,i), strcat('x0', num2str(i)), 'HorizontalAlignment', 'left', 'VerticalAlignment', 'top');
 end
 
-% --- Posizioni finali ---
+% --- Final positions ---
 for i = 1:n_robots
-    plot(Xd(1,i), Xd(2,i), '-x', 'MarkerSize', 8, 'LineWidth', 1.5, 'MarkerFaceColor', colors(i));
+    plot(Xd(1,i), Xd(2,i), '-x', 'MarkerSize', 8, 'LineWidth', 1.5, 'MarkerEdgeColor', colors(i));
+    text(Xd(1,i), Xd(2,i), strcat('xd', num2str(i)), 'HorizontalAlignment', 'left', 'VerticalAlignment', 'top');
 end
 
-% --- Traiettorie (da Simulink o simulazione) ---
+% --- Trajectories (from Simulink simulations) ---
 X1 = squeeze(out.X1.Data);
 X2 = squeeze(out.X2.Data);
 X3 = squeeze(out.X3.Data);
 X4 = squeeze(out.X4.Data);
 X5 = squeeze(out.X5.Data);
 
-% --- Handle delle traiettorie ---
+% --- Handling trajectories ---
 h = gobjects(1, n_robots);
 for i = 1:n_robots
     h(i) = plot(NaN, NaN, colors(i), 'LineWidth', 2);
@@ -68,14 +50,26 @@ end
 %        'Goal1','Goal2','Goal3','Goal4','Goal5', ...
 %        'Traj1','Traj2','Traj3','Traj4','Traj5');
 
-%% === ANIMAZIONE DINAMICA ===
-% (qui si suppone che tutti i vettori Xi abbiano stessa lunghezza)
+% --- System dynamics ---
 for i = 1:length(X1(1,:))
     set(h(1), 'XData', X1(1,1:i), 'YData', X1(2,1:i));
     set(h(2), 'XData', X2(1,1:i), 'YData', X2(2,1:i));
     set(h(3), 'XData', X3(1,1:i), 'YData', X3(2,1:i));
     set(h(4), 'XData', X4(1,1:i), 'YData', X4(2,1:i));
     set(h(5), 'XData', X5(1,1:i), 'YData', X5(2,1:i));
+    drawnow;
+    pause(0.2);
+end
+
+subplot(1, 2, 2); 
+
+grid on;
+
+% --- Graph dynamics ---
+
+for i = 1:length(X1(1,:))
+    [~,p] = create_graph([X1(:,i), X2(:,i), X3(:,i), X4(:,i), X5(:,i)],range, ...
+        'Evolution of robot positions (nodes) and connections (edges)');
     drawnow;
     pause(0.2);
 end
